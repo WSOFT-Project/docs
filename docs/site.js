451c9aec86ad9489f4b98e2b37c0205af5209495
taiseiue <taisei@wsoft.gq>
2022-11-07 22:46:09 +0900

    いろいろ書いたり

--- docs/site.js
+++ docs/site.js
@@ -1,6 +1,1 @@
 document.getElementById("nav-docs").classList.add("active");
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-33M7VJN24C');
