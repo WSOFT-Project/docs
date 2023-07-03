176e05f4befc2288ca0b2d87ec8c79959be98177
taiseiue <taisei@wsoft.ws>
2023-07-03 21:36:08 +0900

    ビルドスクリプト修正

--- build.sh
+++ build.sh
@@ -25,6 +25,6 @@ echo "Replacing WSOFT Common Tags..."
 
 curl -OL https://wsoft.ws/cei
 chmod +x cei
-./cei common/layout.html --target:https://wsoft.ws/common/layout.html --min site
+./cei https://wsoft.ws/common/layout.html --min site
 
 echo "Build completed. Please check /site directory."
