cbe9944e93ba2050ee412f94aa8c75786b43f4b5
taiseiue <taisei@wsoft.ws>
2024-11-28 14:32:35 +0900

    でけた

--- slugify.py
+++ slugify.py
@@ -4,8 +4,8 @@ import unicodedata
 def slugify(value, separator):
     """Slugify a string, making it URL friendly."""
     normalized = unicodedata.normalize('NFKC', value)
-    # Remove leading/trailing whitespace, including Japanese whitespace
-    normalized = re.sub(r'^[ 　]+|[ 　]+$', '', normalized)
+    # Remove leading/trailing whitespace
+    normalized = re.sub(r'^[\s]+|[\s]+$', '', normalized)
     # Replace whitespace with the separator
     normalized = re.sub(r'\s+', separator, normalized)
     # Replace special English grammar characters except dot (・)
